da2d69de8924a0718f2b03859b14df12a4284b82
Adrian Gaudebert <adrian@gaudebert.fr>
2013-07-21 20:40:22 +0200

    Made it work with requirejs, cleaned the code.

--- microevent.js
+++ microevent.js
@@ -8,47 +8,46 @@
  * - create a MicroEventDebug with goodies to debug
  *   - make it safer to use
 */
+define(function () {
+    var MicroEvent = function () {};
+    MicroEvent.prototype = {
+        on: function (event, fct) {
+            this._events = this._events || {};
+            this._events[event] = this._events[event] || [];
+            this._events[event].push(fct);
+        },
+        off: function (event, fct) {
+            this._events = this._events || {};
+            if (event in this._events === false) return;
+            this._events[event].splice(this._events[event].indexOf(fct), 1);
+        },
+        emit: function (event /* , args... */) {
+            this._events = this._events || {};
+            if (event in this._events === false) return;
+            for (var i = 0; i < this._events[event].length; i++) {
+                this._events[event][i].apply(this, Array.prototype.slice.call(arguments, 1));
+            }
+        }
+    };
 
-var MicroEvent	= function(){};
-MicroEvent.prototype	= {
-	on	: function(event, fct){
-		this._events = this._events || {};
-		this._events[event] = this._events[event]	|| [];
-		this._events[event].push(fct);
-	},
-	off	: function(event, fct){
-		this._events = this._events || {};
-		if( event in this._events === false  )	return;
-		this._events[event].splice(this._events[event].indexOf(fct), 1);
-	},
-	emit	: function(event /* , args... */){
-		this._events = this._events || {};
-		if( event in this._events === false  )	return;
-		for(var i = 0; i < this._events[event].length; i++){
-			this._events[event][i].apply(this, Array.prototype.slice.call(arguments, 1));
-		}
-	}
-};
+    /**
+     * mixin will delegate all MicroEvent.js function in the destination object
+     *
+     * - require('MicroEvent').mixin(Foobar) will make Foobar able to use MicroEvent
+     *
+     * @param {Object} the object which will support MicroEvent
+    */
+    MicroEvent.mixin = function (destObject) {
+        var props = ['on', 'off', 'emit'];
+        for (var i = 0; i < props.length; i ++) {
+            if (typeof destObject === 'function') {
+                destObject.prototype[props[i]] = MicroEvent.prototype[props[i]];
+            }
+            else {
+                destObject[props[i]] = MicroEvent.prototype[props[i]];
+            }
+        }
+    }
 
-/**
- * mixin will delegate all MicroEvent.js function in the destination object
- *
- * - require('MicroEvent').mixin(Foobar) will make Foobar able to use MicroEvent
- *
- * @param {Object} the object which will support MicroEvent
-*/
-MicroEvent.mixin	= function(destObject){
-	var props	= ['on', 'off', 'emit'];
-	for(var i = 0; i < props.length; i ++){
-		if( typeof destObject === 'function' ){
-			destObject.prototype[props[i]]	= MicroEvent.prototype[props[i]];
-		}else{
-			destObject[props[i]] = MicroEvent.prototype[props[i]];
-		}
-	}
-}
-
-// export in common js
-if( typeof module !== "undefined" && ('exports' in module)){
-	module.exports	= MicroEvent;
-}
+    return MicroEvent;
+});
